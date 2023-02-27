73d57448020ab321af1b1e9157f78968f7ec6595
BrunoAmadei <brunoamadei98@gmail.com>
2023-02-27 15:26:11 -0300

    created worker

--- Classes/class-01/initial-template/pages/titles/src/factories/cardsFactory.js
+++ Classes/class-01/initial-template/pages/titles/src/factories/cardsFactory.js
@@ -1,6 +1,8 @@
 import CardsController from "./../controllers/cardsController.js"
 import CardsView from "./../views/cardsView.js"
 import CardsService from "./../services/cardsService.js"
+
+const cardListWorker = new Worker(`./src/workers/cardListWorker.js`, { type: "module" })
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initalize() {
