b62469d19854f540a671160364cc812256acfeb5
BrunoAmadei <brunoamadei98@gmail.com>
2023-02-28 11:01:33 -0300

    Create util.js
    supports for different navigators

---
+++ Classes/class-01/initial-template/pages/lib/shared/util.js
@@ -0,0 +1,13 @@
+function supportsWorkerType() {
+    let supports = false
+
+    const tester = {
+        get type() { supports = true }
+    }
+
+    try {
+        new Worker('blob://', tester).terminate()
+    } finally {
+        return supports
+    }
+}
