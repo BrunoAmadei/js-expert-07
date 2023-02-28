d18622eb4bd3ba40fafc53129355415d12f79c12
BrunoAmadei <brunoamadei98@gmail.com>
2023-02-28 11:36:28 -0300

    button added
    button to inicialize eye blink

--- Classes/class-01/initial-template/pages/video-player/src/controller.js
+++ Classes/class-01/initial-template/pages/video-player/src/controller.js
@@ -1,14 +1,29 @@
-export default class Controller{
-    constructor({}){
+export default class Controller {
+    #view
+    #service
+    constructor({ view, service }) {
+        this.#view = view
+        this.#service = service
 
+        this.#view. configureOnBtnClick(this.onBtnStart.bind(this))
     }
 
-    static async initialize(deps){
+    static async initialize(deps) {
         const controller = new Controller(deps)
+        controller.log('not detecting eye blink, click button to start')
+
         return controller.init()
     }
 
-    async init(){
-       
+    async init() {
+
+    }
+
+    log(text) {
+        this.#view.log(`logger: ${text}`)
+    }
+
+    onBtnStart(){
+        this.log('initializing detection...')
     }
 }
