941143ab246f4487d7c818c8347b81d16fee27c0
BrunoAmadei <brunoamadei98@gmail.com>
2023-03-02 09:55:12 -0300

    created canvas element

--- Classes/class-02/initial-template/pages/titles/src/views/handGestureView.js
+++ Classes/class-02/initial-template/pages/titles/src/views/handGestureView.js
@@ -1,13 +1,31 @@
 export default class HandGestureView {
-    loop(fn) {
-      requestAnimationFrame(fn)
-    }
-  
-    scrollPage(top) {
-      scroll({
-        top,
-        behavior: "smooth"
-      })
-    }
-  
+  #handsCanvas = document.querySelector('#hands')
+  #canvasContext = this.#handsCanvas.getContext('2d')
+
+  constructor() {
+    this.#handsCanvas.width = globalThis.screen.availWidth
+    this.#handsCanvas.height = globalThis.screen.availHeight
+
   }
+
+  clear() {
+    this.#canvasContext.clearRect(0, 0, this.#handsCanvas.width, this.#handsCanvas.height)
+  }
+
+  // RECEBENDO AS MÃOS DO TF 
+  drawResults(hands){
+    
+  }
+
+  loop(fn) {
+    requestAnimationFrame(fn)
+  }
+
+  scrollPage(top) {
+    scroll({
+      top,
+      behavior: "smooth"
+    })
+  }
+
+}
