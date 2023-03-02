21d370b2a2cd0e081c04645d0b9c13eee517fb1a
BrunoAmadei <brunoamadei98@gmail.com>
2023-03-02 10:44:27 -0300

    fix path

--- Classes/class-02/initial-template/pages/titles/src/views/handGestureView.js
+++ Classes/class-02/initial-template/pages/titles/src/views/handGestureView.js
@@ -1,45 +1,5 @@
 export default class HandGestureView {
-  #handCanvas = document.getElementById('#hands')
-  #canvasContext = this.#handCanvas.getContext("2d")
-
-  constructor() {
-    this.#handsCanvas.width = globalThis.screen.availWidth
-    this.#handsCanvas.height = globalThis.screen.availHeight
-  }
-
-  clearCanvas() {
-    this.#canvasContext.clearRect(0, 0, this.#canvasContext.width, this.#canvasContext.height)
-  }
-
-  // RECEBENDO AS MÃOS DO TF 
-  drawResults(hands) {
-    for (const { keypoints, handedness } of hands) {
-      if (!keypoints) continue
-
-      this.#canvasContext.fillStyle = handedness === "Left" ? "red" : "green"
-      this.#canvasContext.strokeStyle = "white"
-      this.#canvasContext.lineWidth = 8
-      this.#canvasContext.lineJoin = "round"
-
-      this.#drawJoins(keypoints)
-    }
-  }
-
-  #drwaJoins(keypoints) {
-    for (const { x, y } of keypoints) {
-      this.#canvasContext.beginPath()
-      const newX = x - 2
-      const newY = y - 2
-      const radius = 3
-      const startAngle = 0
-      const endAngle = 2 * Math.PI
-  
-      this.#canvasContext.arc(newX, newY, radius, startAngle, endAngle)
-      this.#canvasContext.fill()
-    }
-  }
-
-  loop(fn) {
+   loop(fn) {
     requestAnimationFrame(fn)
   }
 
